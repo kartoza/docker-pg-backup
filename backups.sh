99476dbf952daa36722b086f09e54c9d40cf6f62
admire <admire@kartoza.com>
2019-02-13 16:42:34 +0200

    Merge branch 'master' of github.com:kartoza/docker-pg-backup
    # Conflicts:
    #	Dockerfile
    
    Add option to dump globals

--- backups.sh
+++ backups.sh
@@ -19,11 +19,19 @@ echo "Backup running to $MYBACKUPDIR" >> /var/log/cron.log
 # Loop through each pg database backing it up
 #
 
-DBLIST=`psql -l | awk '{print $1}' | grep -v "+" | grep -v "Name" | grep -v "List" | grep -v "(" | grep -v "template" | grep -v "postgres" | grep -v "|" | grep -v ":"`
+DBLIST=`psql -l | awk '$1 !~ /[+(|:]|Name|List|template|postgres/ {print $1}'`
 # echo "Databases to backup: ${DBLIST}" >> /var/log/cron.log
 for DB in ${DBLIST}
 do
   echo "Backing up $DB"  >> /var/log/cron.log
   FILENAME=${MYBACKUPDIR}/${DUMPPREFIX}_${DB}.${MYDATE}.dmp
-  pg_dump -Fc -f ${FILENAME} -x -O ${DB}
+  pg_dump -Fc -f ${FILENAME}  ${DB}
+  if [[  -f ${MYBASEDIR}/globals.sql ]]; then
+        rm ${MYBASEDIR}/globals.sql
+        pg_dumpall  --globals-only -f ${MYBASEDIR}/globals.sql
+  else
+        echo "globals not found"
+        pg_dumpall  --globals-only -f ${MYBASEDIR}/globals.sql
+  fi
+
 done
