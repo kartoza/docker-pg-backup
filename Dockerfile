059e5fc1ae006bd8426e0fd4abd5dd774b5f2605
admire <admire@kartoza.com>
2020-10-30 18:43:06 +0200

    First pass at saving to minio

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,22 @@
 FROM kartoza/postgis:13.0
 MAINTAINER tim@kartoza.com
 
-RUN apt-get -y update; apt-get -y --no-install-recommends install  cron && apt-get -y --purge autoremove && apt-get clean \
-&& rm -rf /var/lib/apt/lists/*
+RUN apt-get -y update; apt-get -y --no-install-recommends install  cron s3cmd awscli \
+    && apt-get -y --purge autoremove && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
 RUN touch /var/log/cron.log
+ENV \
+    STORAGE_BACKEND='FILE' \
+    ACCESS_KEY_ID= \
+    SECRET_ACCESS_KEY= \
+    DEFAULT_REGION='us-west-2' \
+    BUCKET='backups' \
+    HOST_BASE= \
+    HOST_BUCKET= \
+    SSL_SECURE='True' \
+    DUMP_ARGS='-Fc' \
+    EXTRA_CONF=
+
 
 ADD scripts /backup-scripts
 RUN chmod 0755 /backup-scripts/*.sh
--- scripts/backups.sh
+++ scripts/backups.sh
@@ -5,18 +5,56 @@ source /pgenv.sh
 #echo "Running with these environment options" >> /var/log/cron.log
 #set | grep PG >> /var/log/cron.log
 
+function minio_config() {
+  if [[ -f /root/.s3cfg ]]; then
+    rm /root/.s3cfg
+  fi
+
+  cat >/root/.s3cfg <<EOF
+host_base = ${HOST_BASE}
+host_bucket = ${HOST_BUCKET}
+bucket_location = ${DEFAULT_REGION}
+use_https = ${SSL_SECURE}
+
+# Setup access keys
+access_key =  ${ACCESS_KEY_ID}
+secret_key = ${SECRET_ACCESS_KEY}
+
+# Enable S3 v4 signature APIs
+signature_v2 = False
+${EXTRA_CONF}
+EOF
+}
+
+
+
 MYDATE=`date +%d-%B-%Y`
 MONTH=$(date +%B)
 YEAR=$(date +%Y)
-MYBASEDIR=/backups
-MYBACKUPDIR=${MYBASEDIR}/${YEAR}/${MONTH}
-mkdir -p ${MYBACKUPDIR}
-cd ${MYBACKUPDIR}
+
+if [[ ${STORAGE_BACKEND} != 'FILE' ]];then
+  MYBASEDIR=/${BUCKET}
+  MYBACKUPDIR=${MYBASEDIR}/${YEAR}/${MONTH}
+  mkdir -p ${MYBACKUPDIR}
+  cd ${MYBACKUPDIR}
+elif [[ ${STORAGE_BACKEND} == 'AWS' ]]; then
+  MYBACKUPDIR=${YEAR}/${MONTH}
+  minio_config
+  s3cmd mb s3://${BUCKET}
+
+fi
 
 echo "Backup running to $MYBACKUPDIR" >> /var/log/cron.log
 
 # Backup globals Always get the latest
-pg_dumpall  --globals-only -f ${MYBASEDIR}/globals.sql
+
+
+if [[ ${STORAGE_BACKEND} == 'FILE' ]];then
+  pg_dumpall  --globals-only -f ${MYBASEDIR}/globals.sql
+else
+  pg_dumpall --globals-only  | s3cmd put - s3://${BUCKET}/globals.sql
+  echo "Sync globals.sql to ${BUCKET} bucket  " >> /var/log/cron.log
+fi
 
 
 # Loop through each pg database backing it up
@@ -27,9 +65,21 @@ do
   if [ -z "${ARCHIVE_FILENAME:-}" ]; then
   	FILENAME=${MYBACKUPDIR}/${DUMPPREFIX}_${DB}.${MYDATE}.dmp
   else
-  	FILENAME=${MYBASEDIR}/"${ARCHIVE_FILENAME}.${DB}.dmp"
+    if [[ ${STORAGE_BACKEND} == 'FILE' ]];then
+  	  FILENAME=${MYBASEDIR}/"${ARCHIVE_FILENAME}.${DB}.dmp"
+  	else
+  	  FILENAME="${ARCHIVE_FILENAME}.${DB}.dmp"
+  	fi
   fi
-  pg_dump -Fc -f ${FILENAME}  ${DB}
+  if [[ ${STORAGE_BACKEND} == 'FILE' ]];then
+      pg_dump ${DUMP_ARGS} -f ${FILENAME}  ${DB}
+      echo "Backing up $FILENAME"  >> /var/log/cron.log
+  else
+      pg_dump ${DUMP_ARGS} ${DB} -f ${FILENAME}
+      s3cmd sync -r  ${FILENAME} s3://${BUCKET}/
+      rm ${FILENAME}
+  fi
+
 done
 
 if [ "${REMOVE_BEFORE:-}" ]; then
