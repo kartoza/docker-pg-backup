5cec892f4e631af84fd6afd8ca35712e3c712da6
Admire Nyakudya <mazano@za-nyakudya-mac.local>
2025-12-15 13:33:46 +0200

    fix unit test

--- scenario_tests/s3/tests/test_restore.py
+++ scenario_tests/s3/tests/test_restore.py
@@ -11,28 +11,35 @@ class TestRestore(unittest.TestCase):
 
     def test_archive_uploaded(self):
         s3_base_path = f"s3://{os.environ.get('BUCKET')}/"
+
         globals_dump_archive = 'globals.sql'
         dump_prefix = os.environ.get('DUMPPREFIX')
-        db_name = 'gis'
-
-        current_archive = datetime.now().strftime(
-            f"{dump_prefix}_{db_name}.%d-%B-%Y-%H-%M.gz"
-        )
 
         proc = subprocess.run([
             's3cmd',
             'ls',
             f"{s3_base_path}{globals_dump_archive}"
         ], capture_output=True)
+
         out_string = proc.stdout.decode('utf-8')
         self.assertTrue(out_string)
         logger.debug(out_string)
 
-        proc = subprocess.run([
-            's3cmd',
-            'ls',
-            f"{s3_base_path}{current_archive}"
-        ], capture_output=True)
-        out_string = proc.stdout.decode('utf-8')
-        self.assertTrue(out_string)
-        logger.debug(out_string)
+        proc = subprocess.run(
+            ["s3cmd", "ls", f"{s3_base_path}"],
+            capture_output=True,
+            text=True
+        )
+        print(proc)
+
+        archives = [
+            line.split()[-1]
+            for line in proc.stdout.splitlines()
+            if line.endswith(".gz") and dump_prefix in line
+        ]
+        print(archives)
+
+        self.assertTrue(archives)
+
+        latest = sorted(archives)[-1]
+        logger.debug(f"Found archive: {latest}")
